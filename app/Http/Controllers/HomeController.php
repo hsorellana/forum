6ab6b1daa6d95a5446ec75905f1c921ecbf4ce42
Ricardo Castillo <rcastillo.developer@gmail.com>
2017-05-20 19:45:46 -0400

    filter route, remove forum

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DevDojo\Chatter\Models\Models;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
-    }
+     public function index($slug = '')
+     {
+         $pagination_results = config('chatter.paginate.num_of_results');
+
+         $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->orderBy('created_at', 'DESC')->paginate($pagination_results);
+         if (isset($slug)) {
+             $category = Models::category()->where('slug', '=', $slug)->first();
+             if (isset($category->id)) {
+                 $discussions = Models::discussion()->with('user')->with('post')->with('postsCount')->with('category')->where('chatter_category_id', '=', $category->id)->orderBy('created_at', 'DESC')->paginate($pagination_results);
+             }
+         }
+
+         $categories = Models::category()->all();
+         $chatter_editor = config('chatter.editor');
+
+         if ($chatter_editor == 'simplemde') {
+             // Dynamically register markdown service provider
+             \App::register('GrahamCampbell\Markdown\MarkdownServiceProvider');
+         }
+
+         return view('home', compact('discussions', 'categories', 'chatter_editor'));
+     }
 }
